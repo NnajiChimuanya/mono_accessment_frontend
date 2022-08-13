724a70c3ce9e097f1e5b958e56bd67196bea6bef
Nnaji Chimuanya <nnajichimuanyua50@gmail.com>
2022-08-13 01:57:44 -0700

    Installed the necessary dependencies to get the app started

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { IconButton, Avatar, Icon } from "@mui/material";
+import MoodIcon from "@mui/icons-material/Mood";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Avatar />
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
