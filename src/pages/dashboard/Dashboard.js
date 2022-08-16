92544bfdaae9d13f04f808838c95fc7e6371831c
Nnaji Chimuanya <nnajichimuanyua50@gmail.com>
2022-08-16 13:07:47 -0700

    Started working on the

--- src/pages/dashboard/Dashboard.js
+++ src/pages/dashboard/Dashboard.js
@@ -1,8 +1,9 @@
 import React from "react";
 import "./dashboard.css";
 import Sidebar from "../../components/sidebar/Sidebar";
-import { Avatar } from "@mui/material";
-import { CreditCard, MoreHoriz } from "@mui/icons-material";
+import { Avatar, IconButton } from "@mui/material";
+import AvatarGroup from "@mui/material/AvatarGroup";
+import { CreditCard, MoreHoriz, PlayCircleFilled } from "@mui/icons-material";
 import {
   BarChart,
   Bar,
@@ -40,7 +41,13 @@ const Dashboard = () => {
             <div className="chart-title">Expense Tracker</div>
             <div className="barchat">
               <div className="barchat-container">
-                <BarChart barGap={5} width={450} height={120} data={data}>
+                <BarChart
+                  defaultShowTooltip
+                  barGap={5}
+                  width={450}
+                  height={120}
+                  data={data}
+                >
                   <Bar barSize={15} dataKey="uv" fill="#dbeff0" />
                 </BarChart>
               </div>
@@ -57,9 +64,208 @@ const Dashboard = () => {
                 <MoreHoriz />
               </div>
             </div>
+            <hr />
+            <div className="transaction-list">
+              <div className="list-item">
+                <div className="item-info">
+                  <PlayCircleFilled />
+                  <div className="item-info-text">
+                    <p className="title"> Netflix subscription</p>
+                    <p className="time">
+                      {" "}
+                      07/08/22 . 08 : 22pm . <span> Credit </span>
+                    </p>
+                  </div>
+                </div>
+                <div className="amount">
+                  <p> -10800 </p>
+                </div>
+              </div>
+              <div className="list-item">
+                <div className="item-info">
+                  <PlayCircleFilled />
+                  <div className="item-info-text">
+                    <p className="title"> Netflix subscription</p>
+                    <p className="time">
+                      {" "}
+                      07/08/22 . 08 : 22pm . <span> Credit </span>
+                    </p>
+                  </div>
+                </div>
+                <div className="amount">
+                  <p> -10800 </p>
+                </div>
+              </div>
+              <div className="list-item">
+                <div className="item-info">
+                  <PlayCircleFilled />
+                  <div className="item-info-text">
+                    <p className="title"> Netflix subscription</p>
+                    <p className="time">
+                      {" "}
+                      07/08/22 . 08 : 22pm . <span> Credit </span>
+                    </p>
+                  </div>
+                </div>
+                <div className="amount">
+                  <p> -10800 </p>
+                </div>
+              </div>
+              <div className="list-item">
+                <div className="item-info">
+                  <PlayCircleFilled />
+                  <div className="item-info-text">
+                    <p className="title"> Netflix subscription</p>
+                    <p className="time">
+                      {" "}
+                      07/08/22 . 08 : 22pm . <span> Credit </span>
+                    </p>
+                  </div>
+                </div>
+                <div className="amount">
+                  <p> -10800 </p>
+                </div>
+              </div>
+              <div className="list-item">
+                <div className="item-info">
+                  <PlayCircleFilled />
+                  <div className="item-info-text">
+                    <p className="title"> Netflix subscription</p>
+                    <p className="time">
+                      {" "}
+                      07/08/22 . 08 : 22pm . <span> Credit </span>
+                    </p>
+                  </div>
+                </div>
+                <div className="amount">
+                  <p> -10800 </p>
+                </div>
+              </div>
+              <div className="list-item">
+                <div className="item-info">
+                  <PlayCircleFilled />
+                  <div className="item-info-text">
+                    <p className="title"> Netflix subscription</p>
+                    <p className="time">
+                      {" "}
+                      07/08/22 . 08 : 22pm . <span> Credit </span>
+                    </p>
+                  </div>
+                </div>
+                <div className="amount">
+                  <p> -10800 </p>
+                </div>
+              </div>
+              <div className="list-item">
+                <div className="item-info">
+                  <PlayCircleFilled />
+                  <div className="item-info-text">
+                    <p className="title"> Netflix subscription</p>
+                    <p className="time">
+                      {" "}
+                      07/08/22 . 08 : 22pm . <span> Credit </span>
+                    </p>
+                  </div>
+                </div>
+                <div className="amount">
+                  <p> -10800 </p>
+                </div>
+              </div>
+              <div className="list-item">
+                <div className="item-info">
+                  <PlayCircleFilled />
+                  <div className="item-info-text">
+                    <p className="title"> Netflix subscription</p>
+                    <p className="time">
+                      {" "}
+                      07/08/22 . 08 : 22pm . <span> Credit </span>
+                    </p>
+                  </div>
+                </div>
+                <div className="amount">
+                  <p> -10800 </p>
+                </div>
+              </div>
+              <div className="list-item">
+                <div className="item-info">
+                  <PlayCircleFilled />
+                  <div className="item-info-text">
+                    <p className="title"> Netflix subscription</p>
+                    <p className="time">
+                      {" "}
+                      07/08/22 . 08 : 22pm . <span> Credit </span>
+                    </p>
+                  </div>
+                </div>
+                <div className="amount">
+                  <p> -10800 </p>
+                </div>
+              </div>
+              <div className="list-item">
+                <div className="item-info">
+                  <PlayCircleFilled />
+                  <div className="item-info-text">
+                    <p className="title"> Netflix subscription</p>
+                    <p className="time">
+                      {" "}
+                      07/08/22 . 08 : 22pm . <span> Credit </span>
+                    </p>
+                  </div>
+                </div>
+                <div className="amount">
+                  <p> -10800 </p>
+                </div>
+              </div>
+              <div className="list-item">
+                <div className="item-info">
+                  <PlayCircleFilled />
+                  <div className="item-info-text">
+                    <p className="title"> Netflix subscription</p>
+                    <p className="time">
+                      {" "}
+                      07/08/22 . 08 : 22pm . <span> Credit </span>
+                    </p>
+                  </div>
+                </div>
+                <div className="amount">
+                  <p> -10800 </p>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
-        <div className="dashboard-main-balance"> Balance</div>
+        <div className="dashboard-main-balance">
+          <div className="total-balance-container">
+            <div className="total-balance">
+              <div className="total-balance-title">
+                <p>TOTAL BALANCE</p>
+              </div>
+              <div className="total-amount">
+                <p>30,000,000</p>
+              </div>
+              <div className="total-amount-text">
+                <p id="text"> Your balance across all Banks</p>
+              </div>
+              <div className="banks-container">
+                <AvatarGroup total={24}>
+                  <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
+                  <Avatar
+                    alt="Travis Howard"
+                    src="/static/images/avatar/2.jpg"
+                  />
+                  <Avatar
+                    alt="Agnes Walker"
+                    src="/static/images/avatar/4.jpg"
+                  />
+                  <Avatar
+                    alt="Trevor Henderson"
+                    src="/static/images/avatar/5.jpg"
+                  />
+                </AvatarGroup>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
